1637f3d33bd809d8f3fc25183fe9fcfa5de310d4
bruno.allison <bruno.allison@live.com>
2018-10-24 23:35:11 -0300

    commit inicial

---
+++ src/main/java/com/brunoallison/challenge/api/TakeHomeCodeChallengeBackEndApplication.java
@@ -0,0 +1,12 @@
+package com.brunoallison.challenge.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeHomeCodeChallengeBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakeHomeCodeChallengeBackEndApplication.class, args);
+	}
+}
