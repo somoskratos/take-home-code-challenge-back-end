62365f02ba4751df852dc1e03940c049d774de56
bruno.allison <bruno.allison@live.com>
2018-10-26 03:46:33 -0300

    criação das regras e tratando os erros

--- src/main/java/com/brunoallison/challenge/api/services/ItemService.java
+++ src/main/java/com/brunoallison/challenge/api/services/ItemService.java
@@ -3,15 +3,15 @@ package com.brunoallison.challenge.api.services;
 import com.brunoallison.challenge.api.models.Itens;
 
 public class ItemService {
-	
+
 	public Itens realizarCalculos(Itens itens) {
 		itens.getItens().forEach((item) -> {
 			Double valorItens = item.getValorUnitario() * item.getQuantidade();
-			
-            item.getTributos().getIcms().calculaBaseDeCalculo(valorItens);
-            item.getTributos().getIcms().calculaValorICMS(valorItens);
-        });
-		
+
+			item.getTributos().getIcms().calculaBaseDeCalculo(valorItens);
+			item.getTributos().getIcms().calculaValorICMS(valorItens);
+		});
+
 		return itens;
 	}
 
